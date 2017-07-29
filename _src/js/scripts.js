6bc8adcfb33ffa672de1e2647c6ac5a8aa121e4a
Silas Burton <silas.burton@gmail.com>
2017-07-29 12:18:15 -0400

    build process

---
+++ _src/js/scripts.js
@@ -0,0 +1,1 @@
+//=require vendor/jquery-3.2.1.min.js
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 'use strict'
 
 var gulp = require('gulp')
+
+var autoprefixer = require('gulp-autoprefixer')
 var browserSync = require('browser-sync').create()
+var include = require('gulp-include')
+var sass = require('gulp-sass')
 var shell = require('gulp-shell')
+var uglify = require('gulp-uglify')
 var watch = require('gulp-watch')
 
 gulp.task('jekyll', shell.task([
@@ -20,17 +25,34 @@ gulp.task('browserSync', function() {
   })
 })
 
+gulp.task('sass', function () {
+  return gulp.src('./_src/scss/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./css'))
+})
+
+gulp.task('scripts', function() {
+  gulp.src('_src/js/scripts.js')
+    .pipe(include())
+      .on('error', console.log)
+    .pipe(uglify())
+    .pipe(gulp.dest('./js'));
+});
+
 gulp.task('versionCSS', shell.task([
-  'md5 -r _site/css/style.css | awk \'{print $1}\' > _data/hash_css.yml'
+  'md5 -r ./css/style.css | awk \'{print $1}\' > _data/hash_css.yml'
 ]))
 
 gulp.task('versionJs', shell.task([
-  'md5 -r _site/js/scripts.js | awk \'{print $1}\' > _data/hash_js.yml'
+  'md5 -r ./js/scripts.js | awk \'{print $1}\' > _data/hash_js.yml'
 ]))
 
-gulp.task('versionWatch', function () {
-  gulp.watch('_site/css/style.css', ['versionCSS'])
-  gulp.watch('_site/js/scripts.js', ['versionJs'])
+gulp.task('watch', function () {
+  gulp.watch('./_src/scss/**/*.scss', ['sass'])
+  gulp.watch('./_src/js/**/*.js', ['scripts'])
+  gulp.watch('./css/style.css', ['versionCSS'])
+  gulp.watch('./_site/js/scripts.js', ['versionJs'])
 })
 
-gulp.task('default', ['jekyll', 'browserSync', 'versionWatch'])
+gulp.task('default', ['jekyll', 'browserSync', 'watch'])
