d1fc4d12c0b45db7eb745819fb4c36aa4283b22e
Silas Burton <silas.burton@gmail.com>
2017-07-29 17:41:30 -0400

    form interactivity

--- _src/js/scripts/contactForm.js
+++ _src/js/scripts/contactForm.js
@@ -1,13 +1,31 @@
 var $contactForm = $('[data-js-contactForm]')
+var $submitButton = $('[data-js-contactFormSubmit]')
+var $formNotification = $('[data-js-contactFormNotification]')
 var emailAddress = $contactForm.attr('data-emailAddress')
+var successMessage = $contactForm.attr('data-successMessage')
 
 $contactForm.submit(function(e) {
   e.preventDefault()
+  $submitButton.attr('disabled', 'true')
+  $submitButton.blur()
+  $submitButton.addClass('working')
 
   $.ajax({
     url: 'https://formspree.io/' + emailAddress, 
     method: 'POST',
     data: $(this).serialize(),
-    dataType: 'json'
+    dataType: 'json',
+    success: function() {
+      $submitButton.attr('value', 'Sent!')
+      $formNotification.addClass('active')
+      $submitButton.removeClass('working')
+      $formNotification.html(successMessage)
+    },
+    error: function() {
+      $submitButton.attr('value', 'Error')
+      $formNotification.addClass('active error')
+      $submitButton.removeClass('working')
+      $formNotification.html('Something went wrong! Please send us an email instead.')
+    }
   })
 })
--- index.md
+++ index.md
@@ -68,6 +68,7 @@ about_us:
 
     We work with a network of designers, developers, and experts in a wide variety of fields such as data science, audience development in order to put together the ideal team for each project.
 contact_title: Contact us
+contact_success_message: "Thanks for your message! We will reply shortly."
 layout: homepage
 ---
 
