a9473a6800e0f8eef2be0a211b8a7536e3f32f93
Silas Burton <silas.burton@gmail.com>
2017-07-29 16:50:05 -0400

    mobile styles

--- _src/js/scripts/stickyHeader.js
+++ _src/js/scripts/stickyHeader.js
@@ -3,12 +3,14 @@ var $siteFooter = $('[data-js-siteFooter]')
 var topOut
 
 window.onscroll = function() {
-  topOut = (window.innerHeight / 100 * 36) - 48
-  if ($(window).scrollTop() >= topOut) {
-    $siteHeader.addClass('fixed')
-    $siteFooter.addClass('fixed')
-  } else {
-    $siteHeader.removeClass('fixed')
-    $siteFooter.removeClass('fixed')
+  if (window.innerWidth >= 768) {
+    topOut = (window.innerHeight / 100 * 36) - 48
+    if ($(window).scrollTop() >= topOut) {
+      $siteHeader.addClass('fixed')
+      $siteFooter.addClass('fixed')
+    } else {
+      $siteHeader.removeClass('fixed')
+      $siteFooter.removeClass('fixed')
+    }
   }
 }
